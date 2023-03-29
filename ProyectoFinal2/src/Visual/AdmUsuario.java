1d633d9b740fa476d8c2518922e98108bc45bfd2
eleazarc799 <eleazarc799@gmail.com>
2023-03-28 21:05:33 -0400

    agregando la clase de administrar usuario y conexion.

---
+++ ProyectoFinal2/src/Visual/AdmUsuario.java
@@ -0,0 +1,78 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.Toolkit;
+
+public class AdmUsuario extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			AdmUsuario dialog = new AdmUsuario();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public AdmUsuario() {
+		setTitle("Administrar Usuario");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(AdmUsuario.class.getResource("/Image/repair.png")));
+		setBounds(100, 100, 700, 600);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JScrollPane scrollPane = new JScrollPane();
+			contentPanel.add(scrollPane, BorderLayout.CENTER);
+			{
+				table = new JTable();
+				scrollPane.setViewportView(table);
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton btnNewButton = new JButton("Eliminar");
+				btnNewButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+					}
+				});
+				{
+					JButton btnNewButton_1 = new JButton("Modificar");
+					buttonPane.add(btnNewButton_1);
+				}
+				buttonPane.add(btnNewButton);
+			}
+			{
+				JButton okButton = new JButton("Aceptar");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+		}
+	}
+
+}
